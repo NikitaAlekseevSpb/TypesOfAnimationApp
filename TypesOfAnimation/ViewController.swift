fcbcfc45ec1e5648fa16cb1649bc302dd322093e
Nik <nik.alekseev.spb@gmail.com>
2021-04-28 00:07:01 +0300

    customized button

--- TypesOfAnimation/ViewController.swift
+++ TypesOfAnimation/ViewController.swift
@@ -10,6 +10,7 @@ import Spring
 class ViewController: UIViewController {
 
     @IBOutlet var animationView: SpringView!
+    @IBOutlet var nameBotton: UIButton!
     
     @IBOutlet var presentLable: UILabel!
     @IBOutlet var curveLabel: UILabel!
@@ -19,13 +20,33 @@ class ViewController: UIViewController {
     
     let animations = Animation.getAnimations()
     
-    private var animationStarted = false
+    private var animationStarted = true
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        nameBotton.setTitle("Run", for: .normal)
+    }
+   
     @IBAction func showAnimation() {
         
+        if animationStarted {
+        guard let animation = animations.randomElement() else {return}
+        animationView.animation = animation.present
+        animationView.curve = animation.curve
+        animationView.force = CGFloat(animation.force)
+        animationView.duration = CGFloat(animation.duration)
+        animationView.delay = CGFloat(animation.delay)
+        animationView.animate()
         
+        presentLable.text = "Present: \(animation.present)"
+        curveLabel.text = "Curve: \(animation.curve)"
+        forseLable.text = "Forse: \(String(format: "%.2f", animation.force))"
+        durationLabel.text = "Duration: \(String(format: "%.2f", animation.duration))"
+        delayLabel.text = "Delay: \(String(format: "%.2f", animation.delay))"
+        //} else {
+            nameBotton.setTitle(animation.present, for: .normal)
+        }
     }
-    
 
 }
 
